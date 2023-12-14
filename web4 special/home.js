8571a9ab5bef8eae52189f922e12006eecfe7966
Your Name <you@example.com>
2023-12-14 08:28:23 +0700

    new

---
+++ web4 special/home.js
@@ -0,0 +1,19 @@
+
+function buy(){
+    alert("Sorry your can not buy yet, Becuz this Web just for Testing");
+}
+function vdo(){
+    alert("Sorry No video data yet in our Vicheka Pizza Shop");
+}
+function food(){
+    alert("Sorry We have no food, Becuz this is just clone web by kak kmeng sna. yul nv?");
+}
+function table(){
+    alert("Sorry We have no table, Becuz this is just clone web by kak kmeng sna. yul nv?");
+}
+function chat(){
+    alert("Send Tv na yii, ot mean back-end te yul?");
+}
+function joch(){
+
+}
